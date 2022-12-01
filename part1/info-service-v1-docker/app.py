283729cd2f8148f4f36b6df20b4b459af6165086
NidhiMishra <nidhimishra2906@gmail.com>
2022-12-01 14:34:00 +0100

    Update app.py

--- part1/info-service-v1-docker/app.py
+++ part1/info-service-v1-docker/app.py
@@ -7,8 +7,6 @@ from sqlalchemy import text
 from sqlalchemy.sql import select
 from flask import request
 import config
-from flask_swagger_ui import get_swaggerui_blueprint
-
 # class Watch:
 #     def __init__(self, row):
 #         self.sku = row[0]
@@ -28,23 +26,8 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def healthCheck(sku=None):
-    return "APi working"
+    return "API is working working"
 
-@app.rout('/static/<path:path>')
-def send_static(path):
-    return send_from_dictionary('static', path)
-    ### swagger specific ###
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-### end swagger specific ###
 @app.route("/watch/",methods=["POST"])
 def addWatch():
     print("add watch")
@@ -59,9 +42,13 @@ def addWatch():
     case_material = request_data["case_material"]
     bracelet_material = request_data["bracelet_material"]
     movement = request_data["movement"]
-    with engine.connect() as conn:
-        result = conn.execute(text("INSERT INTO watches(sku, type, gender, year, dial_material, case_material, bracelet_material, movement) VALUES('"+sku+"','"+watch_type+"','"+gender+"', '"+str(year)+"', '"+dial_material+"', '"+case_material+"', '"+bracelet_material+"', '"+movement+"')"))
-        return str(result)
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("INSERT INTO watches(sku, type, gender, year, dial_material, case_material, bracelet_material, movement) VALUES('"+sku+"','"+watch_type+"','"+gender+"', '"+str(year)+"', '"+dial_material+"', '"+case_material+"', '"+bracelet_material+"', '"+movement+"')"))
+            return str(result)
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
 
 # INSERT INTO watches(sku, type, gender, year, dial_material, case_material, bracelet_material, movement)
 # VALUES('"+sku+"','"+watch_type+"','"+gender+"', '"+year+"', '"+dial_material+"', '"+case_material+"', '"+bracelet_material+"', '"+movement+"');
@@ -69,17 +56,119 @@ def addWatch():
 @app.route("/watch/search/<sku>",methods=["GET"])
 def getSearchedWatch(sku=None):
     print("search get")
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from watches where watches.sku like '"+sku+"'")).fetchall()
-        return str(result)
-            
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("select * from watches where watches.sku like '"+sku+"'")).fetchall()
+            return str(result)
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+
+
 @app.route("/watch/<sku>",methods=["GET"])
 def getWatch(sku=None):
     print("normal get")
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from watches where watches.sku = '"+sku+"'")).fetchone()
-        print(result)
-        return str(result)
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("select * from watches where watches.sku = '"+sku+"'")).fetchone()
+            print(result)
+            return str(result)
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+
+
+@app.route("/watch/delete/<sku>",methods=["POST"])
+def getWatch(sku=None):
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("select * from watches where watches.sku = '"+sku+"'")).fetchone()
+            if len(result) == 1:
+                esult = conn.execute(text("delete * from watches where watches.sku = '"+sku+"'")).fetchone()
+                print(result)
+                return str(result)
+
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+
+
+
+
+"""
+no sql calls
+"""
+
+
+import pymongo
+from pymongo import MongoClient
+# client = MongoClient('localhost', 27017)
+# db = client['project2']
+
+
+client = pymongo.MongoClient("mongodb+srv://root:root1234@cluster0.gyuxkjj.mongodb.net/?w=majority")
+db = client.watches
+collection = db['watches']
+
+@app.route("/watch_mongo/",methods=["POST"])
+def addWatch_mongo():
+    print("add watch")
+    request_data = request.get_json()
+    print(request_data)
+    sku = request_data["sku"]
+    watch_type = request_data["type"]
+    gender = request_data["gender"]
+    year = request_data["year"]
+    print(type(year))
+    dial_material = request_data["dial_material"]
+    case_material = request_data["case_material"]
+    bracelet_material = request_data["bracelet_material"]
+    movement = request_data["movement"]
+    try:
+        collection.insert({"sku":sku, "type":watch_type, "gender":gender, "year":year, "dial_material":dial_material,"case_material":case_material,"bracelet_material":bracelet_material,"movement":movement})
+        return "Done"
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+
+
+@app.route("/watch/search_mongo/<sku>",methods=["GET"])
+def getSearchedWatch_mongo(sku=None):
+    print("search get", sku)
+    res = []
+    try:
+        for x in collection.find({"sku":{"$regex":sku}}):
+            res.append(x)
+            print(x)
+        return str(res)
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+    
+            
+@app.route("/watch_mongo/<sku>",methods=["GET"])
+def getWatch_mongo(sku=None):
+    print("normal get", sku)
+    try:
+        x = collection.find_one({"sku":sku})
+        return str(x)
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+
+@app.route("/watch_mongo/delete/<sku>",methods=["POST"])
+def getWatch_mongo(sku=None):
+    print("normal get", sku)
+    try:
+        x = collection.find_one({"sku":sku})
+        if len(x) == 1:
+            collection.delete({"sku":sku})
+            return "Done"
+    except:
+        print("Something went wrong ")
+        return "Something went wrong"
+
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
