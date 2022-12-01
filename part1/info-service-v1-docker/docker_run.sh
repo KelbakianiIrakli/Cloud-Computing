ef30a48502fc6e4c9be874452b6f5537669cebff
NidhiMishra <nidhimishra2906@gmail.com>
2022-12-01 14:38:02 +0100

    Update docker_run.sh

--- part1/info-service-v1-docker/docker_run.sh
+++ part1/info-service-v1-docker/docker_run.sh
@@ -1,19 +1,8 @@
-#!/bin/env bash
-
-./build.sh
-
-DB_HOST=127.0.0.1
-DB_PORT=3306
-DB_DBNAME=watches
-DB_USER=watches
-DB_PASS=watches
-
-
-
-docker run -d --network=host \
-	-e DB_HOST=$DB_HOST \
-	-e DB_PORT=$DB_PORT \
-	-e DB_DBNAME=$DB_DBNAME \
-	-e DB_USER=$DB_USER \
-	-e DB_PASS=$DB_PASS \
-	info-service-v1
+FROM python:3.11-slim-bullseye
+RUN apt update
+WORKDIR /app
+ADD requirements.txt /app/requirements.txt
+RUN pip install -r /app/requirements.txt
+ADD . /app
+ENV PORT 8080
+CMD ["gunicorn", "app:app", "--config=config.py"]
